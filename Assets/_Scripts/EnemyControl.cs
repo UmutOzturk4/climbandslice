d38f890d45a730f61ccd645a08158119dc8fb329
Umut Öztürk <umutozmail1@gmail.com>
2023-07-18 23:39:26 +0300

    HotErrorFixs

--- Assets/_Scripts/EnemyControl.cs
+++ Assets/_Scripts/EnemyControl.cs
@@ -1,6 +1,6 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class EnemyControl : MonoBehaviour
@@ -12,11 +12,11 @@ public class EnemyControl : MonoBehaviour
     }
     private void Update()
     {
-        if (player.transform.position.x > this.transform.position.x && math.round(player.transform.position.y) == math.round(this.transform.position.y))
+        if (player.transform.position.x > this.transform.position.x && Math.Round(player.transform.position.y) == Math.Round(this.transform.position.y))
         {
             this.transform.rotation = Quaternion.Euler(0, 90, 0);
         }
-        if (player.transform.position.x < this.transform.position.x && math.round(player.transform.position.y) == math.round(this.transform.position.y))
+        if (player.transform.position.x < this.transform.position.x && Math.Round(player.transform.position.y) == Math.Round(this.transform.position.y))
         {
             this.transform.rotation = Quaternion.Euler(0, -90, 0);
         }
